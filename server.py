32d74be2c15a193f6355966353b2210aff9d05f1
Jonna's MacBook <chrisd456@Jonna-Mac-Pro.local>
2024-04-17 04:48:05 -0400

    working pitch navigation

--- server.py
+++ server.py
@@ -6,6 +6,13 @@ from flask import request
 
 app = Flask(__name__)
 
+pitches = [
+    {'name': 'Four-Seam', 'image': 'four-seam.png', 'color': 'red-base', 'path': 'four_seam'},
+    {'name': 'Two-Seam', 'image': 'two-seam.png', 'color': 'orange-base', 'path': 'two_seam'},
+    {'name': 'Slider', 'image': 'slider.png', 'color': 'yellow-base', 'path': 'slider'},
+    {'name': 'Curveball', 'image': 'curveball.png', 'color': 'green-base', 'path': 'curveball'},
+    {'name': 'Changeup', 'image': 'changeup.png', 'color': 'teal-base', 'path': 'changeup'}
+]
 
 # ROUTES
 data = 0
@@ -18,7 +25,22 @@ def home():
 
 @app.route('/lessons', methods=['GET', 'POST'])
 def lessons(): 
-    return render_template('lessons.html')   
+    return render_template('lessons.html', pitches=pitches)
+
+@app.route('/pitches/<pitch_name>')
+def pitch(pitch_name):
+    for pitch in pitches:
+        if pitch['path'] == pitch_name:
+            pitch_data=pitch
+
+    current_index = next((i for i, p in enumerate(pitches) if p['path'] == pitch_name), None)
+    if current_index is not None:
+        next_index = (current_index + 1) % len(pitches)  # This wraps around to the first pitch
+        next_pitch = pitches[next_index]
+    return render_template('pitch_template.html', pitch=pitches[current_index], next_pitch=next_pitch)
+
+
+
 @app.route('/quiz')
 def quiz():
     return render_template('quiz.html')
