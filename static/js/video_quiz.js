e01eee2c25053d64be57ece085b32fd174ac9e10
Sid Vijay <svijay@fredhutch.org>
2024-04-24 01:43:17 -0400

    modified part 2 quiz

--- static/js/video_quiz.js
+++ static/js/video_quiz.js
@@ -6,7 +6,7 @@ function submitQuiz() {
 
     // Placeholder logic for checking answers
     // In reality, you might have a server-side check or a more complex client-side validation
-    var correctAnswers = ["Four-Seam", "Slider", "Curveball", "Two-Seam", "Changeup"];  // Example correct answers
+    var correctAnswers = ["Curveball", "Curveball", "Four-Seam", "Four-Seam", "Changeup", "Changeup", "Slider", "Slider", "Two-Seam", "Two-Seam"];  // Example correct answers
     var score = 0;
     for (var i = 0; i < answers.length; i++) {
         if (answers[i] === correctAnswers[i]) {
