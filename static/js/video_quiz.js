2c3591e4b1e74592ad50bff587805333e3c6d8a5
Sid Vijay <svijay@fredhutch.org>
2024-05-06 22:32:29 -0400

    added pagination for quiz p2 and drag changes for p1

--- static/js/video_quiz.js
+++ static/js/video_quiz.js
@@ -1,22 +1,52 @@
-function submitQuiz() {
-    var answers = [];
-    $('.pitch-select').each(function() {
-        answers.push($(this).val());
-    });
-
-    // Placeholder logic for checking answers
-    // In reality, you might have a server-side check or a more complex client-side validation
-    var correctAnswers = ["Curveball", "Curveball", "Four-Seam", "Four-Seam", "Changeup", "Changeup", "Slider", "Slider", "Two-Seam", "Two-Seam"];  // Example correct answers
-    var score = 0;
-    for (var i = 0; i < answers.length; i++) {
-        if (answers[i] === correctAnswers[i]) {
-            score++;
-        }
-    }
-
-    $('#quiz-result').html('<h3>Your Score: ' + score + ' out of ' + correctAnswers.length + '</h3>');
-}
-
 $(document).ready(function() {
-    // You might want to randomize the order of the videos or options here
+    let currentQuestion = 0;
+    const totalQuestions = $('.video-question').length;
+    $('.video-question').eq(currentQuestion).show();
+
+    updateNavigationButtons();
+
+    window.prevQuestion = function() {
+        if (currentQuestion > 0) {
+            $('.video-question').eq(currentQuestion).hide();
+            currentQuestion--;
+            $('.video-question').eq(currentQuestion).show();
+            updateNavigationButtons();
+        }
+    };
+
+    window.nextQuestion = function() {
+        if (currentQuestion < totalQuestions - 1) {
+            $('.video-question').eq(currentQuestion).hide();
+            currentQuestion++;
+            $('.video-question').eq(currentQuestion).show();
+            updateNavigationButtons();
+        }
+    };
+
+    window.submitQuiz = function() {
+        var answers = [];
+        $('.pitch-select').each(function() {
+            answers.push($(this).val());
+        });
+
+        var correctAnswers = ["Curveball", "Curveball", "Four-Seam", "Four-Seam", "Changeup", "Changeup", "Slider", "Slider", "Two-Seam", "Two-Seam"];
+        var score = 0;
+        for (var i = 0; i < answers.length; i++) {
+            if (answers[i] === correctAnswers[i]) {
+                score++;
+            }
+        }
+
+        $('#quiz-result').html('<h3>Your Score: ' + score + ' out of ' + correctAnswers.length + '</h3>');
+        $('.video-question').hide();
+        $('#prevBtn').hide();
+        $('#nextBtn').hide();
+        $('#submitBtn').hide();
+    };
+
+    function updateNavigationButtons() {
+        $('#prevBtn').toggle(currentQuestion > 0);
+        $('#nextBtn').toggle(currentQuestion < totalQuestions - 1);
+        $('#submitBtn').toggle(currentQuestion === totalQuestions - 1);
+    }
 });
